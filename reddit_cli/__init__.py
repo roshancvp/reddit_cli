2eac2380b5ca188e7dbc9b1c258a5c60394bbe4c
Roshan Prabhakar <roshancvp@gmail.com>
2017-02-09 02:14:28 -0800

    Added click

--- reddit_cli/__init__.py
+++ reddit_cli/__init__.py
@@ -1,2 +1,14 @@
-def hello():
-    return "hello"
+import click
+from termcolor import colored
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(colored('Hello %s!' % name, 'blue'));
+
+if __name__ == '__main__':
+    hello()
