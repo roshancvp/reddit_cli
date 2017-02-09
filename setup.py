5b04e78be9f7bb8d0ede7c00a5c9419cfdff6339
Roshan Prabhakar <roshancvp@gmail.com>
2017-02-09 01:51:28 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='reddit_cli',
+    description='A CLI to browse Reddit.',
+    version='0.1',
+    url='http://github.com/roshancvp/reddit_cli',
+    author='Roshan Prabhakar',
+    author_email='roshancvp@gmail.com',
+    license='MIT',
+)
