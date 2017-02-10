5887281d62c4d4db443910795516fcf74c03e8d0
Roshan Prabhakar <roshancvp@gmail.com>
2017-02-09 22:00:59 -0800

    Added numbers

--- reddit_cli/__init__.py
+++ reddit_cli/__init__.py
@@ -5,6 +5,6 @@ from api import req
 @click.command()
 def cli():
     posts = req.top_posts();
-    click.echo(colored('Welcome to the Reddit CLI', 'blue'))
+    click.echo(colored('Welcome to the Reddit CLI\n', 'blue'))
     for i in range(len(posts)):
-        click.echo(colored(posts[i]['data']['title'], 'green'))
+        click.echo(str(i + 1) + ". " + colored(posts[i]['data']['title'] + "\n", 'green'))
